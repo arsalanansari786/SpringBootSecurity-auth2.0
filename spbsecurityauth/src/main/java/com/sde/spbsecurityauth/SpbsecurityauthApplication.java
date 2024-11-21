6731d705e6a890286b2166e387715eea30c15126
mohdarsalanansari <mohdarsalan.ansari@coraltele.com>
2024-11-21 17:08:59 +0530

    initialize the project

---
+++ spbsecurityauth/src/main/java/com/sde/spbsecurityauth/SpbsecurityauthApplication.java
@@ -0,0 +1,13 @@
+package com.sde.spbsecurityauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbsecurityauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbsecurityauthApplication.class, args);
+	}
+
+}
---
+++ spbsecurityauth/src/test/java/com/sde/spbsecurityauth/SpbsecurityauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sde.spbsecurityauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbsecurityauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
